4ff91ea76a50504ae5bb3a267094f2c9fb100da7
Shyam Bihari Yadav <shyambihariy390@gmail.com>
2026-07-17 10:46:31 +0530

    Time: 31 ms (28.89%), Space: 16 MB (96.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    int count = 0;
-    void solve(vector<vector<char>>& grid, int i, int j, int n, int m)
+    void solve(vector<vector<char>>& grid, int i, int j, int n , int m)
     {
-        if(i>=n || i<0 || j>=m || j<0)
-        {
-            return ;
-        }
-        if(grid[i][j] == '1')
-        {
-            grid[i][j]='0';
+            if(i>=n || i<0 || j>=m || j<0)
+            {
+                return ;
+            }
+            if(grid[i][j] == '1')
+            {
+            grid[i][j] = '0';
             solve(grid, i-1, j, n, m);
             solve(grid, i, j+1, n, m);
             solve(grid, i+1, j, n, m);
             solve(grid, i, j-1, n, m);
-           
-        }
+            }
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        for(int i=0;i<n;i++)
+        int count = 0;
+
+        for(int i = 0 ; i < n ; i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j = 0 ; j < m ; j++)
             {
                 if(grid[i][j] == '1')
                 {
-                 count++;
+                    count++;
+                    solve(grid, i, j, n, m);
                 }
-                solve(grid, i, j, n, m);
             }
         }
-        return count ;
+
+        return count;
         
     }
 };
