f1aa58aa70a5b2231444e97ff85b6ba78d45152d
Shyam Bihari Yadav <shyambihariy390@gmail.com>
2026-08-11 15:02:08 +0530

    Time: 73 ms (10.83%), Space: 153.3 MB (10.92%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        priority_queue<pair<int,int>>pq;
-        vector<int>ans;
-        for(int i=0;i<k;i++)
-        {
-            pq.push({nums[i] , i});
-        }
-        int left = 1;
-        auto [val , ind] = pq.top();
-        ans.push_back(val);
-        if(ind < left)
-        {
-            pq.pop();
-        }
+     int n = nums.size();
+     priority_queue<pair<int, int>>pq;
+     vector<int>ans;
 
-        for(int i=k;i<n;i++)
+     for(int i=0;i<k;i++)
+     {
+        pq.push({nums[i] , i});
+     }
+     auto [val , ind] = pq.top();
+     ans.push_back(val);
+     int left = 1;
+     if(ind < left)
+     {
+        pq.pop();
+     }
+
+     for(int i = k ; i < n ; i++)
+     {
+        pq.push({nums[i] , i});
+        auto [value , index] = pq.top();
+        if(index < left)
         {
-            pq.push({nums[i] , i});
-            auto [value , index] = pq.top();
-            if(index < left)
+            while(index < left)
             {
-                while(index < left)
-                {
-                    pq.pop();
-                    tie(value , index) = pq.top();
-                }
+                pq.pop();
+                tie(value , index) = pq.top();
             }
-            ans.push_back(value);
-            left++;
         }
-        return ans;
+        left++;
+        ans.push_back(value);
+     }
+     return ans;
     }
 };
