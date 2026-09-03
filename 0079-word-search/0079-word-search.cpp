58790b15d9c675910fe49d8fa37891c475f2afb1
Shyam Bihari Yadav <shyambihariy390@gmail.com>
2026-09-04 01:57:39 +0530

    Time: 199 ms (68.71%), Space: 10.7 MB (49.28%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,42 +1,62 @@
+
+
 class Solution {
-public: 
-    bool check=false;
+public:
     vector<vector<int>>vis;
-    bool  dfs(vector<vector<char>>& board, string & word, int r, int c, int key)
+
+    bool checkExist(vector<vector<char>>&board, string& word, int i, int j, int n, int m,  int ind)
     {
-        if(key==word.size())
-        {
-            return true;
-        }        
-        if(r >=board.size() || c >= board[0].size( ) || vis[r][c]==1 || board[r][c]!=word[key])
-        {
+            if(i>=n || i < 0 || j>=m || j<0)
+            {
+                return false;
+            }
+
+         if(vis[i][j] ||  word[ind] != board[i][j])
+         {
             return false;
-        }
-                vis[r][c]=1;//visited
-        bool ans=false;
-        ans=ans ||  dfs(board, word, r, c+1, key+1) ||dfs(board, word, r-1, c, key+1)
-          ||  dfs(board, word, r+1, c, key+1)
-          ||  dfs(board, word, r, c-1, key+1);   //lef
-          vis[r][c]=-1;
-          return ans;                   
-   
+         }
+            if(ind == word.size()-1)
+            {
+                return true;
+            }
+
+       
 
 
+            vis[i][j]=1;
+        bool ans =
+            checkExist(board, word,i-1,j,n,m,  ind+1 ) || // up 
+            checkExist(board, word, i, j+1, n,m, ind+1) || // right 
+            checkExist(board, word, i+1, j, n, m, ind+1) || // down
+            checkExist(board, word, i, j-1, n, m, ind+1); // left
+            vis[i][j]=0;
+         
+
+         
+ 
+
+
+         return ans  ;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        vector<int>path;
-        vis.resize(board.size(), vector<int>(board[0].size(),-1));
-        for(int i=0;i<board.size();i++)
+
+        
+        int n = board.size();
+        int m = board[0].size();
+        vis.assign(n,vector<int>(m,0));
+
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<board[0].size();j++)
+            for(int j=0;j<m;j++)
             {
-                if(        dfs(board, word, i, j, 0))
+              
+                if(word[0] == board[i][j] &&  checkExist(board, word, i, j, n, m , 0) )
                 {
-                    return true;
+                    return true ;
                 }
+             
             }
         }
-
 
         return false;
     }
